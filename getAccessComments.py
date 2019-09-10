8e69e5137c91dce26f5289f71079e5c18cd1cf0f
Zhu Zhu <zzx@uw.edu>
2019-09-09 23:06:27 -0700

    traverse directory for all csv file in folder
    Need to geocode addresses in chunks so not to lose too much work when something goes wrong.

--- getAccessComments.py
+++ getAccessComments.py
@@ -8,8 +8,6 @@ import pandas as pd
 import math, os, geocoder, json, requests
 from pandas.io.json import json_normalize
 import numpy as np
-import concurrent.futures
-from multiprocessing import Process
 
 def getUniqueAccessComments():
     '''
@@ -64,86 +62,89 @@ def getUniqueAccessComments():
              order by count desc
           """
     df = pd.read_sql_query(script, cnxn)
-        
+    
     # standardizing json 
     df['AccessComments'] = df['AccessComments'].replace({'ZIPCODE':'ZIP'}, regex=True)
-    
+     
     print (datetime.datetime.now(),'    Writing to CSV')
     
-    df.to_csv(r'C:\Users\zhuzhux\Desktop\19_017_userAccessCommentAddress\new_comments_12months_Agg.csv') 
+    df.to_csv(r'C:\Users\zhuzhux\Desktop\19_017_userAccessCommentAddress\new_comments_12months_Agg.csv')
     
     print (datetime.datetime.now(),'    Finished exporting to csv')    
     return df
     
 def getLatLong(filename):
     '''
-   
+
     '''
     print (datetime.datetime.now(),'    Geocoding addresses')
-    url = 'http://wa-app-prd-001.wsrb.com/AddressWebServiceInternal/api/address/SearchAddressSingleLine/'
+    t3 = datetime.datetime.now()
     
     df = pd.read_csv(filename)
+    
     name, ext = os.path.splitext(filename)
     
     folder = r'C:\Users\zhuzhux\Desktop\19_017_userAccessCommentAddress\results'
     parts = filename.split("\\")
     newpath = os.path.join(folder, parts[-1])
     name, ext = os.path.splitext(newpath)
-    
-    for i, row in df.iterrows():
-        if df.loc[i, 'AccessComments'].startswith('STREETADDRESS'):
-            temp = df.loc[i,'AccessComments'].replace("STREETADDRESS=", ' ')
-            temp = temp.replace("+", ' ')
-            temp = temp.replace("&CITY=", ' ')
-            temp = temp.replace("&STATE=", ' ')
-            temp = temp.replace("&ZIP=", ' ')
-            temp = temp.replace('%25', ' ')
         
-        if df.loc[i, 'AccessComments'].startswith('{"STREETADDRESS'):
-            temp = json_normalize(json.loads(df.loc[i, 'AccessComments']))
-            cols = ['STREETADDRESS', 'CITY', 'STATE', 'ZIP' ]
-            temp['singleaddress'] = temp[cols].apply(lambda row: ' '.join(row.values.astype(str)), axis =1 )
-            
-            temp=temp['singleaddress']
-            temp = temp.replace({'#':''}, regex=True)
-            temp = temp.replace('0  ', '')
-        
-        if df.loc[i, 'AccessComments'].startswith('{"ZIP'):
-            temp = json_normalize(json.loads(df.loc[i, 'AccessComments']))
-            cols = ['STREETADDRESS', 'CITY', 'STATE', 'ZIP' ]
-            temp['singleaddress'] = temp[cols].apply(lambda row: ' '.join(row.values.astype(str)), axis =1 )
-            
-            temp=temp['singleaddress']
-            temp = temp.replace({'#':''}, regex=True)
+    url = 'http://wa-app-prd-001.wsrb.com/AddressWebServiceInternal/api/address/SearchAddressSingleLine/'
 
-        if df.loc[i, 'AccessComments'].startswith('{"INPUT":'):
-            df.loc[i, 'AccessComments']=df.loc[i, 'AccessComments'].replace('\\T', ' ')
-            temp = json_normalize(json.loads(df.loc[i, 'AccessComments']))['INPUT'][0]
+    try:
+        for i, row in df.iterrows():
+            if df.loc[i, 'AccessComments'].startswith('STREETADDRESS'):
+                temp = df.loc[i,'AccessComments'].replace("STREETADDRESS=", ' ')
+                temp = temp.replace("+", ' ')
+                temp = temp.replace("&CITY=", ' ')
+                temp = temp.replace("&STATE=", ' ')
+                temp = temp.replace("&ZIP=", ' ')
+                temp = temp.replace('%25', ' ')
+            elif df.loc[i, 'AccessComments'].startswith('{"STREETADDRESS'):
+                temp = json_normalize(json.loads(df.loc[i, 'AccessComments']))
+                cols = ['STREETADDRESS', 'CITY', 'STATE', 'ZIP' ]
+                temp['singleaddress'] = temp[cols].apply(lambda row: ' '.join(row.values.astype(str)), axis =1 )
+                temp=temp['singleaddress']
+                temp = temp.replace({'#':''}, regex=True)
+                temp = temp.replace('0  ', '')
+            elif df.loc[i, 'AccessComments'].startswith('{"ZIP'):
+                temp = json_normalize(json.loads(df.loc[i, 'AccessComments']))
+                cols = ['STREETADDRESS', 'CITY', 'STATE', 'ZIP' ]
+                temp['singleaddress'] = temp[cols].apply(lambda row: ' '.join(row.values.astype(str)), axis =1 )
+                temp=temp['singleaddress']
+#               temp = temp.replace({'#':''}, regex=True)
+            elif df.loc[i, 'AccessComments'].startswith('{"INPUT":'):
+                df.loc[i, 'AccessComments']=df.loc[i, 'AccessComments'].replace('\\T', ' ')
+                temp = json_normalize(json.loads(df.loc[i, 'AccessComments']))['INPUT'][0]
+            else:
+                pass
             
-        else:
-            pass
-        
-        payload=" '{}' ".format(temp)
-        payload=payload.replace('0    ', ' ')
+            payload=" '{}' ".format(temp)
+            payload=payload.replace('0    ', ' ')
+            payload=payload.replace('Name: singleaddress, dtype: object', '')
+            headers = {    'content-type':'application/json',}
+            response = requests.post(url, data=payload, headers=headers)
+            response_dict = json.loads(response.text)
+            df_temp=json_normalize(response_dict,'AddressGeocoded')
             
-        headers = {    'content-type':'application/json',}
-        response = requests.post(url, data=payload, headers=headers)
-        response_dict = json.loads(response.text)
-        df_temp=json_normalize(response_dict,'AddressGeocoded')
-        #print(df_temp.AddressLocation[0]['X'], df_temp.AddressLocation[0]['Y'])
-        
-        # handle when return is empty
-        if df_temp.empty or df_temp['AddressLocation'].empty:
-            pass
-        else:
-            df.at[i, 'lat'] = df_temp.AddressLocation[0]['Y']
-            df.at[i, 'long'] = df_temp.AddressLocation[0]['X']
-            df.at[i, 'confidence'] = df_temp.Confidence[0]
-            df.at[i, 'source'] = df_temp.Source[0]
+            if df_temp.empty or df_temp['AddressLocation'].empty:
+                pass
+            else:
+                df.at[i, 'lat'] = df_temp.AddressLocation[0]['Y']
+                df.at[i, 'long'] = df_temp.AddressLocation[0]['X']
+                df.at[i, 'confidence'] = df_temp.Confidence[0]
+                df.at[i, 'source'] = df_temp['Source'][0]
+                
+    except Exception:
+        print('Something went wrong')
+    else:
+        pass
     
     df.to_csv(name + '_latlong' + ext, header=True, index=False, float_format="%.6f")
     print (datetime.datetime.now(),'    Finished geocoding')
-    return df
+    
+    t4 = datetime.datetime.now()
+    return t3, t4, filename
 
 def joinFrames(df_right):
     
@@ -152,37 +153,36 @@ def joinFrames(df_right):
     df_join.to_csv(r'C:\Users\zhuzhux\Desktop\19_017_userAccessCommentAddress\lastday_AccessComments_latlong.csv')
 
 def main():
+    # print date time before processing
     t1 = datetime.datetime.now()
     print (t1)
     
-    df = getUniqueAccessComments()
+    #df = getUniqueAccessComments()
     
-    # hard coding for now so don't need to send query
-    #with concurrent.futures.ProcessPoolExecutor() as executor:
+    directory = r'C:\Users\zhuzhux\Desktop\19_017_userAccessCommentAddress\split_files'
     
-    #df = pd.read_csv(r'C:\Users\zhuzhux\Desktop\19_017_userAccessCommentAddress\test_multiprocessing.csv')
+    data = []
+    col_names = ['startTime', 'endTime', 'filename']
     
-    #df = pd.read_csv(r'C:\Users\zhuzhux\Desktop\19_017_userAccessCommentAddress\split_files_7841\new_comments_12months_Agg{.csv')
-    a = r'C:\Users\zhuzhux\Desktop\19_017_userAccessCommentAddress\split_files\new_comments_12months_Agg_2.csv'
-    b = r'C:\Users\zhuzhux\Desktop\19_017_userAccessCommentAddress\split_files\new_comments_12months_Agg_3.csv'
-    #c = r'C:\Users\zhuzhux\Desktop\19_017_userAccessCommentAddress\split_files\new_comments_12months_Agg_4.csv'
-    #d = r'C:\Users\zhuzhux\Desktop\19_017_userAccessCommentAddress\split_files\new_comments_12months_Agg_5.csv'
+    for filename in os.listdir(directory):
+        if filename.endswith(".csv"):
+            print(os.path.join(directory, filename))
+            filename = os.path.join(directory, filename)
+            t3, t4, name = getLatLong(filename)
+            data.append([t3, t4, name])
+        else:
+            continue
     
-    Process(target=getLatLong, args=a).start()
-    Process(target=getLatLong, args=b).start()    
+    df_time = pd.DataFrame(data, columns = col_names)
     
-#    p1=Process(target = getLatLong(df))
-#    p1.start()
-#    p2=Process(target = getLatLong(df))
-#    p2.start()
-#    p1.join()
-#    p2.join()
-    #df = getLatLong(df)
+    df_time.to_csv(r'C:\Users\zhuzhux\Desktop\19_017_userAccessCommentAddress\run_times_temp.csv')
     
-    #joinFrames(df)
+    joinFrames(df)
     
+    # print date time after processing
     t2 = datetime.datetime.now()
-    print (t2 - t1)
+    #find total time elapsed
+    print ('total run time:               ', t2 - t1)
 
 if __name__ == '__main__':
     main()
